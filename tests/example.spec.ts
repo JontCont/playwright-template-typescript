fa2dd9eda23a3e71afb36d0494c90706655ce22f
ConteMA <52921053+JontCont@users.noreply.github.com>
2024-10-08 23:23:37 +0800

    Update Playwright configuration and add screenshots to example tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -5,6 +5,7 @@ test('has title', async ({ page }) => {
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
+  await page.screenshot({ path: './playwright-report-images/screenshot_1.png' });
 });
 
 test('get started link', async ({ page }) => {
@@ -15,4 +16,5 @@ test('get started link', async ({ page }) => {
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await page.screenshot({ path: './playwright-report-images/screenshot_2.png' });
 });
